38279569289e0fc4c39eb99fbc1e4059a18b27f4
Oscar González <osdagoso@hotmail.com>
2018-04-16 01:25:13 -0500

    Fixed bug in Remove module and implemented it in main

--- Desarrollo/Remove.h
+++ Desarrollo/Remove.h
@@ -13,11 +13,11 @@
      public:
          void removePhrases(vector<int> indexPhrases, vector<string> &listPhrases) {
              for(int index : indexPhrases) {
-                listPhrases.erase(listPhrases.begin() + index);
+                listPhrases.erase(listPhrases.begin() + index - 1);
              }
          }
          void cleanIndexList(vector<int> &indexPhrases) {
-             sort(indexPhrases.begin(), indexPhrases.end(), greater<string>());
+             sort(indexPhrases.begin(), indexPhrases.end(), greater<int>());
              indexPhrases.erase(unique(indexPhrases.begin(), indexPhrases.end()), indexPhrases.end());
          }
          void selectPhrasesToRemove(vector<string> &listPhrases) {
@@ -30,18 +30,22 @@
 				}
              cout << endl << "Introduce la cantidad de lineas que desees eliminar: ";
              cin >> cantRemove;
-             cout << "Escribe los numeros de linea: " << endl;
-             for (int i = 0; i < cantRemove; i++) {
-                int index;
-                cin >> index;
-                while (index < 1 || index > listPhrases.size()) {
-                    cout << index << " es un indice invalido. Intentalo de nuevo." << endl;
+
+             if (cantRemove > 0) {
+                 cout << "Escribe los numeros de linea: " << endl;
+                 for (int i = 0; i < cantRemove; i++) {
+                    int index;
                     cin >> index;
-                }
-                indexPhrases.push_back(index);
+                    while (index < 1 || (size_t)index > listPhrases.size()) {
+                        cout << index << " es un indice invalido. Intentalo de nuevo." << endl;
+                        cin >> index;
+                    }
+                    indexPhrases.push_back(index);
+                 }
+                 cleanIndexList(indexPhrases);
+                 removePhrases(indexPhrases, listPhrases);
              }
-             cleanIndexList(indexPhrases);
-             removePhrases(indexPhrases, listPhrases);
+             cout << endl;
          }
          static Remove *instance() {
              if (!rInstance)
